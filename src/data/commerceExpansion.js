fa7f9beeb4d02b35b1e7abe8e2a6c8d6d164a07e
infosmitsircommerce-eng <infosmitsircommerce@gmail.com>
2026-09-04 20:21:04 +0530

    Add college and Commerce exam expansion structure

---
+++ src/data/commerceExpansion.js
@@ -0,0 +1,62 @@
+export const bcomSemesters = [
+  {
+    semester: 1,
+    subjects: ['Financial Accounting', 'Business Organisation & Management', 'Business Economics', 'Business Communication', 'Business Mathematics / Quantitative Techniques', 'Digital / Computer Applications'],
+  },
+  {
+    semester: 2,
+    subjects: ['Corporate Accounting', 'Business Law', 'Macroeconomics / Business Economics', 'Human Resource Management', 'Business Statistics', 'E-Commerce / Information Technology'],
+  },
+  {
+    semester: 3,
+    subjects: ['Cost Accounting', 'Company Law', 'Income Tax', 'Marketing Management', 'Banking & Insurance', 'Entrepreneurship Development'],
+  },
+  {
+    semester: 4,
+    subjects: ['Management Accounting', 'Auditing', 'GST / Indirect Tax', 'Financial Management', 'Research Methods / Statistics', 'International Business'],
+  },
+  {
+    semester: 5,
+    subjects: ['Advanced Accounting', 'Corporate Tax Planning', 'Financial Markets / Investment', 'Management / HR / Marketing Elective', 'Banking / Insurance Elective', 'Project / Internship'],
+  },
+  {
+    semester: 6,
+    subjects: ['Advanced Corporate Accounting', 'Auditing & Assurance', 'Financial Services', 'Business Analytics / Data Skills', 'International Finance / Trade', 'Project / Dissertation'],
+  },
+];
+
+export const mcomSemesters = [
+  {
+    semester: 1,
+    subjects: ['Managerial Economics', 'Advanced Financial Accounting', 'Business Environment', 'Organisational Behaviour', 'Business Statistics', 'Research Methodology'],
+  },
+  {
+    semester: 2,
+    subjects: ['Corporate Finance', 'Marketing Management', 'Human Resource Management', 'Advanced Cost & Management Accounting', 'Business Law / Corporate Law', 'Research Methods & Data Analysis'],
+  },
+  {
+    semester: 3,
+    subjects: ['International Business', 'Strategic Management', 'Tax Planning', 'Banking & Financial Services', 'Security Analysis / Investment Management', 'Specialisation Elective'],
+  },
+  {
+    semester: 4,
+    subjects: ['Corporate Governance', 'Advanced Auditing', 'Financial Markets', 'Business Analytics', 'Specialisation Elective', 'Project / Dissertation'],
+  },
+];
+
+export const commerceExamUnits = [
+  'Business Environment and International Business',
+  'Accounting and Auditing',
+  'Business Economics',
+  'Business Finance',
+  'Business Statistics and Research Methods',
+  'Business Management and Human Resource Management',
+  'Banking and Financial Institutions',
+  'Marketing Management',
+  'Legal Aspects of Business',
+  'Income-tax and Corporate Tax Planning',
+];
+
+export const collegeResourceTypes = ['Notes & PDFs', 'MCQs', 'Important Questions', 'PYQs', 'Revision Sheets'];
+
+export const examResourceTypes = ['Official Syllabus', 'Unit-wise Notes', 'Unit-wise MCQs', 'Previous Papers', 'Mock Tests', 'Revision'];
