e6e920cea49cd47bc3ad60dcdb17b5b3b255b440
infosmitsircommerce-eng <infosmitsircommerce@gmail.com>
2026-09-04 14:45:56 +0530

    Defer noncritical site enhancements after first paint

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -1,24 +1,67 @@
+import { lazy, Suspense, useEffect, useState } from 'react';
 import { useLocation } from 'react-router-dom';
 import Navbar from './Navbar';
 import Footer from './Footer';
 import MobileBottomBar from '../ui/MobileBottomBar';
 import MobileHeader from '../ui/MobileHeader';
-import ScrollToTop from '../ui/ScrollToTop';
-import CursorSpotlight from '../ui/CursorSpotlight';
-import ScrollProgressBar from '../ui/ScrollProgressBar';
-import GlobalStudySearch from '../ui/GlobalStudySearch';
-import CloudSyncBridge from '../ui/CloudSyncBridge';
-import AnalyticsTracker from '../ui/AnalyticsTracker';
-import ChapterProgressTracker from '../ui/ChapterProgressTracker';
-import ChapterPracticeStrip from '../ui/ChapterPracticeStrip';
-import ContextualToolLinks from '../ui/ContextualToolLinks';
-import TopicalAuthorityLinks from '../ui/TopicalAuthorityLinks';
-import MarksRecoveryNudge from '../ui/MarksRecoveryNudge';
-import StudentReferralNudge from '../ui/StudentReferralNudge';
 import PilotHreflang from '../ui/PilotHreflang';
-import DemoNudge from '../leads/DemoNudge';
 import { isLightRoute } from '../../lib/theme';
 import { isAdEligiblePath } from '../../lib/adPolicy';
+
+const ScrollToTop = lazy(() => import('../ui/ScrollToTop'));
+const CursorSpotlight = lazy(() => import('../ui/CursorSpotlight'));
+const ScrollProgressBar = lazy(() => import('../ui/ScrollProgressBar'));
+const GlobalStudySearch = lazy(() => import('../ui/GlobalStudySearch'));
+const CloudSyncBridge = lazy(() => import('../ui/CloudSyncBridge'));
+const AnalyticsTracker = lazy(() => import('../ui/AnalyticsTracker'));
+const ChapterProgressTracker = lazy(() => import('../ui/ChapterProgressTracker'));
+const ChapterPracticeStrip = lazy(() => import('../ui/ChapterPracticeStrip'));
+const ContextualToolLinks = lazy(() => import('../ui/ContextualToolLinks'));
+const TopicalAuthorityLinks = lazy(() => import('../ui/TopicalAuthorityLinks'));
+const MarksRecoveryNudge = lazy(() => import('../ui/MarksRecoveryNudge'));
+const StudentReferralNudge = lazy(() => import('../ui/StudentReferralNudge'));
+const DemoNudge = lazy(() => import('../leads/DemoNudge'));
+
+function DeferredEnhancements() {
+  const [ready, setReady] = useState(false);
+
+  useEffect(() => {
+    let idleId;
+    let timerId;
+    const reveal = () => setReady(true);
+
+    if ('requestIdleCallback' in window) {
+      idleId = window.requestIdleCallback(reveal, { timeout: 1200 });
+    } else {
+      timerId = window.setTimeout(reveal, 650);
+    }
+
+    return () => {
+      if (idleId) window.cancelIdleCallback?.(idleId);
+      if (timerId) window.clearTimeout(timerId);
+    };
+  }, []);
+
+  if (!ready) return null;
+
+  return (
+    <Suspense fallback={null}>
+      <AnalyticsTracker />
+      <ScrollProgressBar />
+      <CursorSpotlight />
+      <GlobalStudySearch />
+      <CloudSyncBridge />
+      <ChapterProgressTracker />
+      <ContextualToolLinks />
+      <ChapterPracticeStrip />
+      <TopicalAuthorityLinks />
+      <MarksRecoveryNudge />
+      <StudentReferralNudge />
+      <DemoNudge />
+      <ScrollToTop />
+    </Suspense>
+  );
+}
 
 export default function Layout({ children }) {
   const { pathname } = useLocation();
@@ -37,27 +80,15 @@ export default function Layout({ children }) {
       >
         Skip to main content
       </a>
-      <AnalyticsTracker />
       <PilotHreflang />
-      <ScrollProgressBar />
-      <CursorSpotlight />
       <Navbar />
       <MobileHeader />
-      <GlobalStudySearch />
-      <CloudSyncBridge />
-      <ChapterProgressTracker />
       <main id="main-content" tabIndex="-1" className="flex-1 pt-0 lg:pt-20 pb-20 lg:pb-0">
         {children}
       </main>
-      <ContextualToolLinks />
-      <ChapterPracticeStrip />
-      <TopicalAuthorityLinks />
-      <MarksRecoveryNudge />
-      <StudentReferralNudge />
       <Footer />
       <MobileBottomBar />
-      <DemoNudge />
-      <ScrollToTop />
+      <DeferredEnhancements />
     </div>
   );
 }
