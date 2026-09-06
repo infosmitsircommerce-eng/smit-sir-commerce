bf6c6757693600d6e98c685e6799915625870945
infosmitsircommerce-eng <infosmitsircommerce@gmail.com>
2026-09-06 13:53:42 +0530

    Simplify homepage flow around student resources

--- src/components/home/HomeBelowFold.jsx
+++ src/components/home/HomeBelowFold.jsx
@@ -1,41 +1,28 @@
-import CommerceJourneyPreview from './CommerceJourneyPreview';
-import LearningPhilosophy from './LearningPhilosophy';
-import TrustLayer from './TrustLayer';
-import MehsanaGrowthCampaign from './MehsanaGrowthCampaign';
-import SeoDiscoveryLinks from './SeoDiscoveryLinks';
-import CommerceToolsPreview from './CommerceToolsPreview';
-import MarksRecoveryCTA from './MarksRecoveryCTA';
-import StatsSection from './StatsSection';
-import CoursesPreview from './CoursesPreview';
-import AboutSection from './AboutSection';
+import StudentResourceGateway from './StudentResourceGateway';
 import FreeResources from './FreeResources';
+import CommerceToolsPreview from './CommerceToolsPreview';
 import DailyPracticeCTA from './DailyPracticeCTA';
 import StudyCoachCTA from './StudyCoachCTA';
-import BatchCTA from './BatchCTA';
+import StudentJourney from './StudentJourney';
+import LearningPhilosophy from './LearningPhilosophy';
+import TrustLayer from './TrustLayer';
 import FAQSection from './FAQSection';
 import ContactCTA from './ContactCTA';
-import StudentJourney from './StudentJourney';
+import SeoDiscoveryLinks from './SeoDiscoveryLinks';
 import SectionDivider from '../ui/SectionDivider';
 
 export default function HomeBelowFold() {
   return (
     <>
-      <CommerceJourneyPreview />
-      <LearningPhilosophy />
-      <TrustLayer />
-      <MehsanaGrowthCampaign />
-      <MarksRecoveryCTA />
+      <StudentResourceGateway />
+      <FreeResources />
       <CommerceToolsPreview />
-      <SeoDiscoveryLinks />
-      <StatsSection />
-      <StudentJourney />
       <DailyPracticeCTA />
       <StudyCoachCTA />
-      <CoursesPreview />
-      <SectionDivider />
-      <AboutSection />
-      <FreeResources />
-      <BatchCTA />
+      <StudentJourney />
+      <LearningPhilosophy />
+      <TrustLayer />
+      <SeoDiscoveryLinks />
       <SectionDivider />
       <FAQSection />
       <ContactCTA />
