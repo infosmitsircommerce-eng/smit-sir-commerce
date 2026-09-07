431b4a42355314bd1183a493c07dbe954041be73
infosmitsircommerce-eng <infosmitsircommerce@gmail.com>
2026-09-07 16:23:32 +0530

    Restore original homepage below fold sections

--- src/components/home/HomeBelowFold.jsx
+++ src/components/home/HomeBelowFold.jsx
@@ -1,28 +1,41 @@
-import StudentResourceGateway from './StudentResourceGateway';
-import FreeResources from './FreeResources';
-import CommerceToolsPreview from './CommerceToolsPreview';
-import DailyPracticeCTA from './DailyPracticeCTA';
-import StudyCoachCTA from './StudyCoachCTA';
-import StudentJourney from './StudentJourney';
+import CommerceJourneyPreview from './CommerceJourneyPreview';
 import LearningPhilosophy from './LearningPhilosophy';
 import TrustLayer from './TrustLayer';
+import MehsanaGrowthCampaign from './MehsanaGrowthCampaign';
+import SeoDiscoveryLinks from './SeoDiscoveryLinks';
+import CommerceToolsPreview from './CommerceToolsPreview';
+import MarksRecoveryCTA from './MarksRecoveryCTA';
+import StatsSection from './StatsSection';
+import CoursesPreview from './CoursesPreview';
+import AboutSection from './AboutSection';
+import FreeResources from './FreeResources';
+import DailyPracticeCTA from './DailyPracticeCTA';
+import StudyCoachCTA from './StudyCoachCTA';
+import BatchCTA from './BatchCTA';
 import FAQSection from './FAQSection';
 import ContactCTA from './ContactCTA';
-import SeoDiscoveryLinks from './SeoDiscoveryLinks';
+import StudentJourney from './StudentJourney';
 import SectionDivider from '../ui/SectionDivider';
 
 export default function HomeBelowFold() {
   return (
     <>
-      <StudentResourceGateway />
-      <FreeResources />
-      <CommerceToolsPreview />
-      <DailyPracticeCTA />
-      <StudyCoachCTA />
-      <StudentJourney />
+      <CommerceJourneyPreview />
       <LearningPhilosophy />
       <TrustLayer />
+      <MehsanaGrowthCampaign />
+      <MarksRecoveryCTA />
+      <CommerceToolsPreview />
       <SeoDiscoveryLinks />
+      <StatsSection />
+      <StudentJourney />
+      <DailyPracticeCTA />
+      <StudyCoachCTA />
+      <CoursesPreview />
+      <SectionDivider />
+      <AboutSection />
+      <FreeResources />
+      <BatchCTA />
       <SectionDivider />
       <FAQSection />
       <ContactCTA />
