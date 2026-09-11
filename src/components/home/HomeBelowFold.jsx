d1b700a46a22850e077bc72648a75084c9ef1394
infosmitsircommerce-eng <infosmitsircommerce@gmail.com>
2026-09-11 07:30:47 +0530

    feat: add Teacher Studio revenue flow (src/components/home/HomeBelowFold.jsx)

--- src/components/home/HomeBelowFold.jsx
+++ src/components/home/HomeBelowFold.jsx
@@ -1,21 +1,22 @@
-import CommerceJourneyPreview from './CommerceJourneyPreview';
-import LearningPhilosophy from './LearningPhilosophy';
-import TrustLayer from './TrustLayer';
-import MehsanaGrowthCampaign from './MehsanaGrowthCampaign';
-import SeoDiscoveryLinks from './SeoDiscoveryLinks';
-import CommerceToolsPreview from './CommerceToolsPreview';
-import MarksRecoveryCTA from './MarksRecoveryCTA';
-import StatsSection from './StatsSection';
-import CoursesPreview from './CoursesPreview';
-import AboutSection from './AboutSection';
-import FreeResources from './FreeResources';
-import DailyPracticeCTA from './DailyPracticeCTA';
-import StudyCoachCTA from './StudyCoachCTA';
-import BatchCTA from './BatchCTA';
-import FAQSection from './FAQSection';
-import ContactCTA from './ContactCTA';
-import StudentJourney from './StudentJourney';
-import SectionDivider from '../ui/SectionDivider';
+import CommerceJourneyPreview from "./CommerceJourneyPreview";
+import LearningPhilosophy from "./LearningPhilosophy";
+import TrustLayer from "./TrustLayer";
+import MehsanaGrowthCampaign from "./MehsanaGrowthCampaign";
+import SeoDiscoveryLinks from "./SeoDiscoveryLinks";
+import CommerceToolsPreview from "./CommerceToolsPreview";
+import MarksRecoveryCTA from "./MarksRecoveryCTA";
+import StatsSection from "./StatsSection";
+import CoursesPreview from "./CoursesPreview";
+import AboutSection from "./AboutSection";
+import FreeResources from "./FreeResources";
+import DailyPracticeCTA from "./DailyPracticeCTA";
+import StudyCoachCTA from "./StudyCoachCTA";
+import BatchCTA from "./BatchCTA";
+import FAQSection from "./FAQSection";
+import ContactCTA from "./ContactCTA";
+import StudentJourney from "./StudentJourney";
+import SectionDivider from "../ui/SectionDivider";
+import TeacherStudioPromo from "./TeacherStudioPromo";
 
 export default function HomeBelowFold() {
   return (
@@ -23,6 +24,7 @@ export default function HomeBelowFold() {
       <CommerceJourneyPreview />
       <LearningPhilosophy />
       <TrustLayer />
+      <TeacherStudioPromo />
       <MehsanaGrowthCampaign />
       <MarksRecoveryCTA />
       <CommerceToolsPreview />
