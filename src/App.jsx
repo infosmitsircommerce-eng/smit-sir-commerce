4b17764aa96d0d85b7281f67bb90ed9ae647afcf
infosmitsircommerce-eng <infosmitsircommerce@gmail.com>
2026-09-22 15:11:19 +0530

    Enable global English Gujarati language switching

--- src/App.jsx
+++ src/App.jsx
@@ -4,18 +4,21 @@ import AppErrorBoundary from "./components/ui/AppErrorBoundary";
 import { AuthProvider } from "./context/AuthContext";
 import { AnimatedRoutes } from "./routes/AppRoutes";
 import RouteSEO from "./routes/RouteSEO";
+import { LanguageProvider } from "./i18n/LanguageContext";
 
 export default function App() {
   return (
     <HelmetProvider>
-      <BrowserRouter future={{ v7_startTransition: true, v7_relativeSplatPath: true }}>
-        <AuthProvider>
-          <AppErrorBoundary>
-            <RouteSEO />
-            <AnimatedRoutes />
-          </AppErrorBoundary>
-        </AuthProvider>
-      </BrowserRouter>
+      <LanguageProvider>
+        <BrowserRouter future={{ v7_startTransition: true, v7_relativeSplatPath: true }}>
+          <AuthProvider>
+            <AppErrorBoundary>
+              <RouteSEO />
+              <AnimatedRoutes />
+            </AppErrorBoundary>
+          </AuthProvider>
+        </BrowserRouter>
+      </LanguageProvider>
     </HelmetProvider>
   );
 }
